520f22260d773ea10952d045243da7ab3767edf4
MasumaJaffery <masumajaffery1@gmail.com>
2023-12-13 20:58:18 +0500

    Greetings by M

--- src/components/greetings.jsx
+++ src/components/greetings.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+const currentDate = new Date();
+let time = currentDate.getHours();
+let greeting = "";
+if(time >= 0 && time <= 12) {
+  greeting = "Buenas Dais";
+} else if(time >= 12 && time <= 24) {
+  greeting = "Buenos Noches";
+} else {
+  greeting = "Salam";
+}
+
 const Greeting = () => {
   return (
-    <p className="text-blue-500">Salam React!</p>
+    <>
+    <h1 className="text-blue-500">Hello, {greeting}</h1>
+    </>
   );
 };
 
 export default Greeting;
+
+/*** Tada! Feliz Codificacion */
