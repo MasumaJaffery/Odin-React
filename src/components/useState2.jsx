0c2c1eba703b8f300bff7a5eef8bfbc4c283a1fb
MasumaJaffery <masumajaffery1@gmail.com>
2023-12-14 22:25:23 +0500

    Update Styles

--- src/components/useState2.jsx
+++ src/components/useState2.jsx
@@ -12,13 +12,32 @@ const form = () => {
       e.preventDefault();
   }
  return (
-   <div className="flex flex-col justify-center items-center">
-    <form onSubmit={handleSubmit}>
-     <input type = "text" name="name" placeholder="Name" value={formData.name} onChange={handleInput} />
-     <input type = "email" name="email" placeholder="Email" value={formData.email} onChange={handleInput} />
-     <button type = "submit">Submit</button>
+    <div className="flex flex-col justify-center items-center">
+    <form onSubmit={handleSubmit} className="max-w-xs bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={formData.name}
+        onChange={handleInput}
+        className="mb-4 px-3 py-2 rounded-md border focus:outline-none focus:border-blue-500"
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleInput}
+        className="mb-4 px-3 py-2 rounded-md border focus:outline-none focus:border-blue-500"
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Submit
+      </button>
     </form>
-   </div>
+  </div>  
  );
 };
 export default form;
